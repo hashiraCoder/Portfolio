8208eaa2f7e0965ca0459171a063b2a74959091c
hashiraCoder <shivam.code@gmail.com>
2026-03-20 00:05:54 +0530

    adding project link

--- client/src/data.jsx
+++ client/src/data.jsx
@@ -12,7 +12,7 @@ export const socialLinks = [
 ];
 
 export const projectData = [
-  { title: "Modern Portfolio", desc: "A full-stack MERN portfolio.", stack: ["React", "Node.js", "MongoDB"], live: "#", github: "https://github.com/hashiraCoder/Portfolio/tree/master/client"},
+  { title: "Modern Portfolio", desc: "A full-stack MERN portfolio.", stack: ["React", "Node.js", "MongoDB"], live: "https://portfolio-taupe-theta-8ean7sd4hi.vercel.app/", github: "https://github.com/hashiraCoder/Portfolio/tree/master/client"},
   // { title: "Modern Portfolio", desc: "A full-stack MERN portfolio.", stack: ["React", "Node.js", "MongoDB"], live: "#", github: "#", img: "https://via.placeholder.com/400x300" }
 { title: "Pathfinder", desc: "A MERN DSA based pathfinder.", stack: ["React", "Node.js", "TailwindCss"], live: "https://pathfinder-2-d-grid.vercel.app/", github: "https://github.com/hashiraCoder/Pathfinder-2D-grid"}
 ];
